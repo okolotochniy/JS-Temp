af77c290796fdde0c9c5d12a623bba1de38f6fc3
DIMA <batsurin@mail.ru>
2022-11-20 23:42:41 +0300

    timer

---
+++ webcademy-js-6-hours-course/main.js
@@ -0,0 +1,12 @@
+const timer = document.querySelector('#counter');
+const buttonStart = document.querySelector('#start');
+const buttonPause = document.querySelector('#pause');
+const buttonReset = document.querySelector('#reset');
+
+buttonStart.addEventListener('click', startCount());
+
+
+
+function startCount() {
+    setInterval(1000, )
+}
